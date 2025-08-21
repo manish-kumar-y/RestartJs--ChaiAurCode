5c0b5ce1bbd7c9065b46d8306d7532ce933f8d0e
Manish Kumar <130752379+manish-kumar-y@users.noreply.github.com>
2025-08-21 06:19:49 +0000

    second commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1234;
+let accountEmail = "manish@info.com";
+var accountPass = "12345";
+branchName = "Patna";
+// console.table([accountEmail,accountPass,branchName]);
+
+
+// prefer not to use var because of block or function Scope 
+// not define any value without variable because this is not a good practice
+
+
+// accountId = 3324;   // not changable
+
+accountEmail= "info@manish.com";
+accountPass = "2345678";
+branchName = "Lakhishrai";
+console.log(accountId);
+console.table([accountEmail,accountPass,branchName]);
