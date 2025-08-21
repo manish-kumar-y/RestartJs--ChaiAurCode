cdfcee038439b997399fcb653f7433a662fbc530
Manish Kumar <130752379+manish-kumar-y@users.noreply.github.com>
2025-08-21 07:41:26 +0000

    convert variable to another variable

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,36 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+
+/*
+33 => 33
+"33" => 33
+"33abc" => NaN
+true => 1 ; false => 0
+
+
+*/
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let valueInString = String(score);
+
+
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+// let value = 1; => true
+// let value = 0 ; => false
+// let value = "Manish"; => true
+// let value = null; => false
+// let value; => false
+
+// let valueInBoolean = Boolean(value);
+
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean)
