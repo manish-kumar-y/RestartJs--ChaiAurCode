04f151b8ad01e5d82aeaa4237d2eda69c44f902a
Manish Kumar <130752379+manish-kumar-y@users.noreply.github.com>
2025-08-21 06:53:07 +0000

    dataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,15 @@
+let name = "Manish";
+let num = 12345;
+let bool = true;
+let dis;
+let length = null;
+
+
+console.log(typeof "name");
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof dis);
+console.log(typeof length);
+
+
+
