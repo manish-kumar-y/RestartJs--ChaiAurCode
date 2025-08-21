d47c461918d66e1ff3199aa7114af7498865b1f8
Manish Kumar <130752379+manish-kumar-y@users.noreply.github.com>
2025-08-21 23:58:38 +0000

    compare two diffrent or same datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(1 > 2);
+// console.log(2 >=3);
+// console.log(2 == 1);
+// console.log(5 >= 3);
+
+
+// console.log("02" > 1);
+// console.log("2" > 1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log (null >= 1);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0 );
+// console.log(undefined < 0);
+// console.log(undefined > 0 );
+// console.log(undefined === 0 );
+
+// ===
+
+
+
+console.log("2" === 1);
+
+// For more information goto mdn
+
+
