a093c71f4d87e62654c74963d981e871fc9a8bc8
Manish Kumar <130752379+manish-kumar-y@users.noreply.github.com>
2025-08-23 06:28:42 +0000

    Number function and Math function and trick

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,92 @@
+const num = 400;
+// const num1 = new Number(1000);
+
+// console.log(num);
+// console.log(num1);
+
+
+// console.log(num.toString().length);
+// console.log(num.toString());
+// console.log(typeof (num.toString()));
+
+// console.log(num.toFixed(2));
+
+// console.log(num.toPrecision(4));
+
+
+// const hundreds = 10000000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.POSITIVE_INFINITY);
+// console.log(1/0);
+// console.log(Number.NEGATIVE_INFINITY);
+// console.log(-1/0);
+// console.log(Number.NaN);
+// console.log(Number.isNaN(Number.NaN));
+// console.log("abc"/2);
+
+
+// console.log(Number.EPSILON);
+// console.log(0.1 + 0.2 === 0.3);
+// console.log((0.1 + 0.2) === 0.3);
+// console.log(Math.abs());
+// console.log(Math.abs((0.1+0.2)-0.3) < Number.EPSILON);
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+
+
+// +++++++++++++++++++++++++++++++++  Math   +++++++++++++++++++++++++++
+
+/*
+
+console.log(Math.round(4.7));
+console.log(Math.round(4.2));
+console.log(Math.floor(4.7));
+console.log(Math.ceil(4.2));
+console.log(Math.trunc(4.9));
+
+
+*/
+
+// const num3 = 5.2
+// console.log(Math.round(num3));
+
+
+// console.log(Math.random());
+
+// console.log((Math.random() * 10) + 1);
+
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+// const min = 1;
+// const max = 6;
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+// console.log(Math.max(5,12,2,18,6));
+// console.log(Math.min(5,12,6,18,15,2));
+// console.log(Math.abs(-5));  //=> only change negetive value to positive value
+// console.log(Math.pow(2,3));  //=> value 2 to the power 3 .
+// console.log(Math.sqrt(16));  //=> square of value 
+// console.log(Math.cbrt(27));
+
+
+
+console.log(Math.PI);
+console.log(Math.E);
+
+
+
