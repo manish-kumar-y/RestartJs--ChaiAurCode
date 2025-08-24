cfc4938b7c7a01d0e2e894d036a5b982e01a285f
Manish Kumar <130752379+manish-kumar-y@users.noreply.github.com>
2025-08-24 11:01:12 +0000

    Arrays function practices

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,84 @@
-const arry = [12,14.7,24,53];
-const arr = new Array(53,2,82,13);
+const arry = [12,14,7,24,53,7];
+// const arr = new Array(53,2,82,13);
+// console.log(arry);
+// console.log(arr);
+// console.log(arr[1]);
+
+
+/*
+
+arry.push("manish");
 console.log(arry);
-console.log(arr);
+
+arry.pop();
+console.log(arry);
+
+arry.unshift(19);
+console.log(arry);
+
+arry.shift();
+console.log(arry);
+
+*/
+
+
+// console.log(arry.indexOf(7));
+// console.log(arry.lastIndexOf(7));
+// console.log(arry.includes(9));
+// console.log(arry.find(n=>n>4));  //=> here find greater then 4 nearest value you also try less and other function
+// console.log(arry.findIndex(n=> n > 7));   //=> here find index of greter then 7 nearest you also try different value or diferent function here
+
+
+
+let num = [2,4,6,8,9,5,8];
+// let mulValue =  num.map(n => n*2);
+// console.log(mulValue);
+// console.log(num.map(n=>n%2==0));   //=>here they give boolean value
+// console.log(num.filter(n=>n%2==0));
+// console.log(num.reduce((a,b)=>a+b,0));  //=>add all value a = 0 and b = indexOf[0] after that a = add a+b and b = index[1] .......
+
+// const num1 = num.forEach(n=>console.log(n));
+// console.log(num1);
+
+
+// console.log(num.sort());
+
+// console.log(num.reverse());
+
+
+/*
+
+const myArr = [12,5,34,54,81,21,2,45];
+console.log("A", myArr);
+console.log(myArr.slice(1,4));
+console.log("B", myArr);
+
+console.log(myArr.splice(1,4));
+console.log("C", myArr);
+
+*/
+
+/*
+
+let a = [23,53,12,43];
+let b = [18,54,23,45];
+
+const c = a.concat(b);
+console.log(c);
+
+const d = a.join("-");
+console.log(d);
+
+console.log(typeof d);
+
+
+*/
+
+
+const str = "apple,Banana".split(",");
+console.log(str);
+console.log(typeof str);
+
+
+
 
