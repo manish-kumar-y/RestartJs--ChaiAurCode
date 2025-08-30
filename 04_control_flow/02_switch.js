b52607c024f2a0d6a0e2cf84d756218b146352de
Manish Kumar <130752379+manish-kumar-y@users.noreply.github.com>
2025-08-30 02:24:05 +0000

    Switch Case Study

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,69 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const day = 5;
+// switch (day) {
+//     case 0:
+//         console.log("Sunday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thuresday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+
+//     default:
+//         console.log("Invalid Value...");
+    
+//         break;
+// }
+
+
+const days = "Friday";
+switch (days) {
+    case "Sunday":
+        console.log(0);
+        break;
+    case "Monday":
+        console.log(1);
+        break;
+    case "Tuesday":
+        console.log(2);
+        break;
+    case "Wednesday":
+        console.log(3);
+        break;
+    case "Thuresday":
+        console.log();
+        break;
+    case "Friday":
+        console.log(5);
+        break;
+    case "Saturday":
+        console.log(6);
+        break;
+
+    default:
+        console.log("Invalid Day...");
+    
+        break;
+}
