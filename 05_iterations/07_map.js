cb1ad766eae984129d1f236027572dce91691fda
Manish Kumar <130752379+manish-kumar-y@users.noreply.github.com>
2025-09-04 09:32:32 +0000

    map function print practice

---
+++ 05_iterations/07_map.js
@@ -0,0 +1,13 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+// let newNum = myNums.map((num)=>{
+//     return num + 10 ;
+// })
+// console.log(newNum);
+
+
+
+// let newNum = myNums
+//                   .map((value)=>value*10)
+//                   .map((value)=>value + 2)
+//                   .filter((value)=>value >50)
+// console.log(newNum);
